ea3dd216d3c621ad4b1a64c2379f85bb81137d3a
Don Allingham <dallingham@gmail.com>
2012-08-22 10:48:20 -0600

    Added support for generation of the rw1s1 type (read/write, 1 shot on write of a 1)

--- regenerate/writers/verilog_reg_def.py
+++ regenerate/writers/verilog_reg_def.py
@@ -53,6 +53,40 @@ endmodule
 
 endmodule
 """,
+    "rw1s1" : """module %(MODULE)s_rw1s1_reg #(parameter WIDTH = 1)
+   (
+    input                  CLK,      // Clock
+    input                  RSTn,     // Reset
+    input                  BE,       // Byte Enable
+    input                  WE,       // Write Strobe
+    input [WIDTH-1:0]      DI,       // Data In
+    input [WIDTH-1:0]      RVAL,     // Reset Value
+    output reg [WIDTH-1:0] DO,       // Data Out
+    output reg             DO_1S     // One Shot
+   );
+
+   always @(posedge CLK or %(RESET_EDGE)s RSTn) begin
+      if (%(RESET_CONDITION)sRSTn) begin
+         DO <= RVAL;
+      end else begin
+         if (WE & %(BE_LEVEL)sBE) begin
+            DO <= DI;
+         end else begin
+            DO <= DO;
+         end
+      end
+   end
+
+   always @(posedge CLK or %(RESET_EDGE)s RSTn) begin
+      if (%(RESET_CONDITION)sRSTn) begin
+         DO_1S <= 1'b0;
+      end else begin
+         DO_1S <= WE & %(BE_LEVEL)sBE && DI != {pWidth{1'b0}};
+      end
+   end
+
+endmodule
+""",
     "rwld" : """module %(MODULE)s_rwld_reg #(parameter WIDTH = 1)
    (
     input                  CLK,      // Clock
@@ -128,15 +162,20 @@ endmodule
    always @(posedge CLK or %(RESET_EDGE)s RSTn) begin
       if (%(RESET_CONDITION)sRSTn) begin
          DO <= RVAL;
-         DO_1S <= 1'b0;
       end else begin
          if (WE & %(BE_LEVEL)sBE) begin
             DO <= DI;
-            DO_1S <= DI;
          end else begin
             DO <= (LD) ? IN : DO;
-            DO_1S <= 1'b0;
          end
+      end
+   end
+
+   always @(posedge CLK or %(RESET_EDGE)s RSTn) begin
+      if (%(RESET_CONDITION)sRSTn) begin
+         DO_1S <= 1'b0;
+      end else begin
+         DO_1S <= WE && %(BE_LEVEL)sBE && DI != {pWidth{1'b0}};
       end
    end
 
@@ -303,7 +342,7 @@ endmodule
       if (%(RESET_CONDITION)sRSTn) begin
          DO_1S <= 1'b0;
       end else begin
-         DO_1S <= WE & %(BE_LEVEL)sBE & (|(DI));
+         DO_1S <= WE && %(BE_LEVEL)sBE && DI != {pWidth{1'b0}};
       end
    end
 
