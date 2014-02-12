bc1db46b23d4b95b4b841bc12136020066aeeb71
Don Allingham <don.allingham@dothill.com>
2014-02-12 11:46:09 -0700

    Fixed write_only registers so that wide registers work and one shot properly generated

--- mk_regs/verilog_reg.v
+++ mk_regs/verilog_reg.v
@@ -778,7 +778,7 @@ module %(MODULE)s_wo_reg #(
    reg [WIDTH-1:0] ws;
    reg [WIDTH-1:0] ws_d;
 
-   assign DO = ws & !ws_d;
+   assign DO = ws & ~ws_d;
 
    always @(posedge CLK or %(RESET_EDGE)s RSTn) begin
       if (%(RESET_CONDITION)sRSTn) begin
@@ -787,6 +787,8 @@ module %(MODULE)s_wo_reg #(
       end else begin
          if (WE & %(BE_LEVEL)sBE) begin
             ws <= DI;
+         end else begin
+            ws <= {WIDTH{1'b0}};
          end
          ws_d <= ws;
       end
--- mk_regs/verilog_reg_def.py
+++ mk_regs/verilog_reg_def.py
@@ -759,7 +759,7 @@ endmodule
    reg [WIDTH-1:0] ws;
    reg [WIDTH-1:0] ws_d;
 
-   assign DO = ws & !ws_d;
+   assign DO = ws & ~ws_d;
 
    always @(posedge CLK or %(RESET_EDGE)s RSTn) begin
       if (%(RESET_CONDITION)sRSTn) begin
@@ -768,6 +768,8 @@ endmodule
       end else begin
          if (WE & %(BE_LEVEL)sBE) begin
             ws <= DI;
+         end else begin
+            ws <= {WIDTH{1'b0}};
          end
          ws_d <= ws;
       end
