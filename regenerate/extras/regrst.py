01ee2145cfdc8be0aae55416f2005b72058df6a6
Don Allingham <dallingham@gmail.com>
2017-07-17 13:10:27 -0600

    Cleaned up regrst'

--- regenerate/extras/regrst.py
+++ regenerate/extras/regrst.py
@@ -272,20 +272,16 @@ class RegisterRst:
         Returns the definition of the register in RestructuredText format
         """
         o = StringIO()
-        rlen = len(self._reg.register_name) + 2
 
-        o.write(".. _%s:\n\n" % self.refname(self._reg.register_name))
+        self.str_title(o)
 
-        o.write(self._reg.register_name)
-        o.write("\n%s\n\n" % ('_' * rlen))
-        o.write("%s\n\n" %
-                self._reg.description.encode('ascii', 'replace'))
+        self.str_overview(o)
 
         if self._reg.ram_size < 32:  # Temporary hack
-            self._write_bit_fields(o)
+            self.str_bit_fields(o)
 
         if self._show_defines:
-            self._write_defines(o, True, False)
+            self.str_defines(o, True, False)
 
         o.write(text)
         return o.getvalue()
@@ -302,12 +298,59 @@ class RegisterRst:
                              norm_name(self._reg.register_name),
                              norm_name(name))
 
-    def _write_bit_fields(self, o):
-        o.write("Bit fields\n+++++++++++++++++++++++++++\n\n")
+
+    def str_title(self, o=None):
+        ret_str = False
+        
+        if o is None:
+            o = StringIO()
+            ret_str = True
+
+        o.write(".. _%s:\n\n" % self.refname(self._reg.register_name))
+            
+        if ret_str:
+            return o.getvalue()
+
+    def str_title(self, o=None):
+        ret_str = False
+        
+        if o is None:
+            o = StringIO()
+            ret_str = True
+
+        rlen = len(self._reg.register_name) + 2
+        o.write(".. _%s:\n\n" % self.refname(self._reg.register_name))
+        o.write(self._reg.register_name)
+        o.write("\n%s\n\n" % ('_' * rlen))
+            
+        if ret_str:
+            return o.getvalue()
+
+    def str_overview(self, o=None):
+        ret_str = False
+        
+        if o is None:
+            o = StringIO()
+            ret_str = True
+
+        o.write("%s\n\n" %
+                self._reg.description.encode('ascii', 'replace'))
+            
+        if ret_str:
+            return o.getvalue()
+        
+    def str_bit_fields(self, o=None):
+
+        ret_str = False
+        
+        if o is None:
+            o = StringIO()
+            ret_str = True
+
         o.write(".. list-table::\n")
         o.write("   :widths: 8, 10, 7, 25, 50\n")
         if self._bootstrap:
-            o.write("   :class: table table-striped table-condensed\n")
+            o.write("   :class: table table-bordered table-striped table-condensed\n")
         else:
             o.write("   :class: bit-table\n")
         o.write("   :header-rows: 1\n\n")
@@ -380,9 +423,30 @@ class RegisterRst:
             o.write("\n\n")
             o.write(descr)
             o.write("\n\n")
+            
+        if ret_str:
+            return o.getvalue()
+        
+    def _write_bit_fields(self, o):
+
+        o.write("Bit fields\n+++++++++++++++++++++++++++\n\n")
+        self.str_bit_fields(o)
+
                     
     def _write_defines(self, o, use_uvm=True, use_id=True):
 
+        o.write("\n\nAddresses\n+++++++++++++++++++++++\n\n")
+        self.str_defines(o, use_uvm, use_id)
+
+
+    def str_defines(self, o=None, use_uvm=True, use_id=True):
+
+        ret_str = False
+        
+        if o is None:
+            o = StringIO()
+            ret_str = True
+    
         x_addr_maps = self._prj.get_address_maps()
         instances = in_groups(self._regset_name, self._prj)
         addr_maps = set([])
@@ -396,7 +460,6 @@ class RegisterRst:
         if not addr_maps:
             return
 
-        o.write("\n\nAddresses\n+++++++++++++++++++++++\n\n")
         if not in_groups(self._regset_name, self._prj):
             o.write(
                 ".. WARNING::\n   Register set was not added to any groups\n\n")
@@ -452,6 +515,9 @@ class RegisterRst:
 
         o.write("\n\n")
 
+        if ret_str:
+            return o.getvalue()
+        
     def _addr_entry(self, o, inst, use_uvm, use_id, addr_maps,
                     grp_inst, group_index, index):
 
