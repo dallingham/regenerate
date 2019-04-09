13529314861790c2c5cb81d918bd27440aa3a978
Don Allingham <dallingham@gmail.com>
2019-04-08 20:42:49 -0600

    Added file

---
+++ regenerate/db/addrmap.py
@@ -0,0 +1,8 @@
+class AddrMapData (object):
+
+    def __init__(self, name, base, width, fixed, uvm):
+        self.name = name
+        self.base = base
+        self.width = width
+        self.fixed = fixed
+        self.uvm = uvm
