1b7059b3ffa1550675585b07b802d24d1ddce959
Don Allingham <dallingham@gmail.com>
2021-03-29 17:17:37 -0600

    Work on UVM regs

--- regenerate/writers/uvm_reg_block.py
+++ regenerate/writers/uvm_reg_block.py
@@ -114,16 +114,19 @@ class UVMRegBlockRegisters(WriterBase):
         data_set = []
         group_maps = self._build_group_maps()
         for dbase in self.get_used_databases():
+            print("***", self._project.block_insts)
             for blk_inst in self._project.block_insts:
                 used = set()
                 for rset_cont in self._project.blocks[
                     blk_inst.block
                 ].block.regsets:
+                    print("...", rset_cont, dbase.regset.set_name)
                     grp_name = rset_cont
                     if (
                         grp_name == dbase.regset.set_name
                         and grp_name not in used
                     ):
+                        print("@@@", grp_name, group_maps)
                         used.add(grp_name)
                         if grp_name in group_maps:
                             data_set.append(
@@ -133,6 +136,8 @@ class UVMRegBlockRegisters(WriterBase):
                                     group_maps[grp_name],
                                 )
                             )
+                            print(data_set)
+        print(data_set)
         return data_set
 
     def get_used_databases(self) -> Set[RegSetContainer]:
